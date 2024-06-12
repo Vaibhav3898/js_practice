3dac93f383249d8c7a94e6a1d0ad7fb08ea792c5
Vainhav3898 <155623051+Vainhav3898@users.noreply.github.com>
2024-06-12 10:44:05 +0000

    data types

--- 01_Basics1/02_DataTypes.js
+++ 01_Basics1/02_DataTypes.js
@@ -1,3 +1,23 @@
 "use strict"; 
 // treat all js code as newer version
 // alter (3+3) we r using node js not a browser
+console.log(3+3);
+// cpde readability shoulb be high
+console.log("Vaibhav")
+let name = "Vaibhav"
+let age = 23
+let state;
+
+// Data Type
+// number - 2 to the power 53
+// bigint - it is used to high number as like in treding
+// String - " " #Always in Colon 
+// Boolean - True / False 
+// NUll - Standard value 
+// undefined is a declared variable that is not initialize
+// Symbol - unique 
+// object 
+console.log(typeof undefined)
+// undefined
+console.log(typeof null);
+// object
