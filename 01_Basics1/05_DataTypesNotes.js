90bf08d0838aac3acb45b9c3f6af965d05e7b2a9
Vainhav3898 <155623051+Vainhav3898@users.noreply.github.com>
2024-06-13 07:33:09 +0000

    data types notes

---
+++ 01_Basics1/05_DataTypesNotes.js
@@ -0,0 +1,26 @@
+// Data Types 
+// Primitive Data Types 
+// 7Types
+//  number, string, Boolean , null,  undefined , symbol & Bigint 
+
+const score = 100
+const scoreValue = 100.3
+const isloggedIn = false
+const outsideTemp = null
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+const bignumber = 3478583489n
+
+// Non Primitive (Reference)
+// Arrays , object , function 
+
+// Arrays 
+const heros = ["srk", "rutik", "ajay"];
+
+// object 
+let myObj = {
+    name: "Vaibhav",
+    age: 22,
+}
