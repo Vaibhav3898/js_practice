e3be91b16f65f9d8e69c188b2cda25a7e496cae3
Vainhav3898 <155623051+Vainhav3898@users.noreply.github.com>
2024-06-13 04:28:00 +0000

    Data Type Conversion

--- 01_Basics1/03_DataTypesConversion.js
+++ 01_Basics1/03_DataTypesConversion.js
@@ -3,8 +3,8 @@ console.log(typeof score);
 console.log(typeof(score))
 
 let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 
 // "33" => 33
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "hitesh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+ console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -23,8 +23,8 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
 // *********************** Operations ***********************
 
@@ -32,25 +32,25 @@ let value = 3
 let negValue = -value
 console.log(negValue);
 
-// console.log(2 + 2)
-// console.log(2-2)
-// console.log(2*3)
-// console.log(2**3)
-// console.log(2/3)
-// console.log(5%3)
+console.log(2 + 2)
+console.log(2-2)
+console.log(2*3)
+console.log(2**3)
+console.log(2/3)
+console.log(5%3)
 
 let str1 = "Hello"
 let str2 = " Vaibhav"
 let str3 = str1 + str2
 
-// console.log(str3)
+console.log(str3)
 
-// console.log("1" + 2)
-// console.log(1 + "2")
-// console.log("1" + 2 + 2)
-// console.log(1+ 2 + "3")
-// console.log(+true)
-// console.log(+" ")
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1+ 2 + "3")
+console.log(+true)
+console.log(+" ")
 
 let num1 , num2 , num3
 num1 = num2 = num3
