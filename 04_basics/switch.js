8776e44d5a42c32e5ac842697077c919f44d642c
Vaibhav3898 <155623051+Vaibhav3898@users.noreply.github.com>
2024-07-04 03:58:22 +0000

    switch case

---
+++ 04_basics/switch.js
@@ -0,0 +1,53 @@
+// syntax 
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+// 
+
+// program :-
+
+const monthNu = 3;
+switch (monthNu) {
+    case 1:
+        console.log("januavary")
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("april")
+        break;
+    default:
+        console.log("Default executed")
+        break;
+}
+
+// prog 2 = 
+
+const month = "march"
+
+switch (month){
+    case 'jan':
+        console.log("jan");
+        break;
+    case 'feb':
+        console.log("feb")
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("april")
+        break;
+    default:
+        console.log("default exicuted");
+        break;
+}
