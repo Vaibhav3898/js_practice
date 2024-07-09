02ea3a6d9425346d3ebc0f652b55efbef85da084
Vaibhva Shinde <155623051+Vaibhav3898@users.noreply.github.com>
2024-07-09 18:36:47 +0000

    key in

---
+++ iteration/four.js
@@ -0,0 +1,23 @@
+const myObject = {
+    js:'javaScript',
+    cpp:'C++',
+    rb:'ruby',
+    swift:'swift by apple'
+}
+for (const key in myObject){
+    console.log(`${key} shortcut is for ${myObject[key]}`)
+}
+
+const programming = ['js','rb','py','java','cpp']
+for (const key in programming){
+    console.log(programming[key])
+}
+
+const map = new Map()
+map.set("In","India")
+map.set("USA","United State India")
+map.set("fr","france")
+map.set("In","India")
+for (const key in map){
+    console.log(key)
+}
